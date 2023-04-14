1bfa3bb695d54dcb724b6c381209c73faa2086fa
brtkllo <105305466+brtkllo@users.noreply.github.com>
2023-04-14 19:02:14 +0200

    Update timesincerf2.js

--- timesincerf2.js
+++ timesincerf2.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 16, 2022 12:00:00").getTime();
+var countDownDate = new Date("Apr 9, 2023 19:15:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
